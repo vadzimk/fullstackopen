f048772657ca43739ab3aa26f1db73e44a488976
Vadzim <vadzimk@hotmail.com>
2020-12-27 05:26:17 +0000

    completed 1.14 anecdotes step3

--- part1_anecdotes/src/index.js
+++ part1_anecdotes/src/index.js
@@ -1,32 +1,53 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-
+const Stat = ({maxVotes, mostVotes}) => {
+    if (maxVotes) {
+        return (
+            <div>
+                <h3>Anecdote with most votes</h3>
+                {mostVotes}<br/>
+                has {maxVotes} votes
+            </div>
+        )
+    } else {
+        return null
+    }
+}
 const App = (props) => {
 
     const [selected, setSelected] = useState(0)
-
     const [votes, setVote] = useState(new Array(anecdotes.length).fill(0))  // zero filled array
+
+    const mostVotesIndex = votes.indexOf(votes.reduce((x, y) => Math.max(x, y)))
+    const maxVotes = votes[mostVotesIndex]
+    const mostVotes = anecdotes[mostVotesIndex]
+
+    // console.log("mostVotesIndex", mostVotesIndex)
+
     // console.log('votes', votes)
     const handleClick = () => {
-        const newValue = Math.floor(Math.random() * anecdotes.length )
+        const newValue = Math.floor(Math.random() * anecdotes.length)
         setSelected(newValue)
     }
 
-    const handleVote = ()=>{
+    const handleVote = () => {
         // correct way of updating component's state stored in mutable objects is to make a copy of the object
         const votesCopy = [...votes]
-        votesCopy[selected]+=1
+        votesCopy[selected] += 1
         setVote(votesCopy)
     }
     return (
         <div>
             <div>
+                <h3>Andecdote of the day</h3>
                 {props.anecdotes[selected]}<br/>
                 has {votes[selected]} votes
             </div>
             <button onClick={handleVote}>vote</button>
             <button onClick={handleClick}>next anecdote</button>
+            <Stat {...{maxVotes, mostVotes}}/>
+
         </div>
     )
 }
